e03da56c2724df4851e07e746e0abdd29cee0247
Pyy05 <2911026958@qq.com>
2026-05-28 22:28:15 +0800

    提交全部更改

---
+++ user-admin-system/src/main/java/com/team/user_admin_system/UserAdminSystemApplication.java
@@ -0,0 +1,13 @@
+package com.team.user_admin_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdminSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminSystemApplication.class, args);
+	}
+
+}
