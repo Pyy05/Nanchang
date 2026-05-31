7852b791d7532b157a178ec84965402e853f2eb5
calm1130 <2647955649@qq.com>
2026-05-31 20:23:44 +0800

    添加图片

--- user-admin-system/src/main/java/com/team/user_admin_system/module/controller/EventController.java
+++ user-admin-system/src/main/java/com/team/user_admin_system/module/controller/EventController.java
@@ -1,7 +1,7 @@
 package com.team.user_admin_system.module.controller;
 
 import com.team.user_admin_system.module.entity.Event;
-import com.team.user_admin_system.module.repository.EventRepository;
+import com.team.user_admin_system.module.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,18 +15,16 @@ import java.util.List;
 public class EventController {
 
     @Autowired
-    private EventRepository eventRepository;
+    private EventService eventService;
 
     @GetMapping("/list")
     public List<Event> getEvents(
             @RequestParam(required = false) String dynasty
     ) {
-        // 没传分类 → 返回全部
-        if (dynasty == null || dynasty.trim().isEmpty()) {
-            return eventRepository.findAll();
+        // 关键：当参数是 null 或者 "全部时期" 时，返回所有数据
+        if (dynasty == null || "全部时期".equals(dynasty)) {
+            return eventService.listAll(); // 查全部
         }
-
-        // 按朝代筛选（你数据库里是 dynasty 字段）
-        return eventRepository.findByDynasty(dynasty);
+        return eventService.listByCategory(dynasty); // 按朝代筛选
     }
 }
